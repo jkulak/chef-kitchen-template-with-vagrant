2a33fa618b56beae5525bddd0cdba9ce69b7f0b5
Jakub Kułak <jakub.kulak@gmail.com>
2016-09-08 17:07:44 -0500

    Clean up defeault recipe

--- site-cookbooks/jku-local/recipes/default.rb
+++ site-cookbooks/jku-local/recipes/default.rb
@@ -1,27 +1,14 @@
-#
-# Cookbook Name:: multipurpose
+# Cookbook Name:: jku-local
 # Recipe:: default
-#
-# Copyright 2016, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
 
 include_recipe 'jku-common::default'
-
 include_recipe 'jku-local::apache2'
 include_recipe 'jku-local::php'
 include_recipe 'jku-local::performance_tester'
-
 # include_recipe 'jku-local::mysql_server'
-
-
 # include_recipe 'jku-local::adminer'
-#
 include_recipe 'jku-local::nodejs'
 include_recipe 'jku-local::nodejs_development'
-
 # include_recipe 'jku-local::mongodb3'
 include_recipe 'jku-local::mongodb'
-
 include_recipe 'jku-local::project_lol'
